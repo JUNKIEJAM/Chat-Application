bf1010992a7cab45e53ea8df5983a5d86c19557d
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-02-02 19:09:20 +0530

    Corrections

--- node-server/index.js
+++ node-server/index.js
@@ -1,22 +1,28 @@
-//Node server which will handle socket io connections
+// Node server which will handle socket io connections
+const io = require('socket.io')(8000)
 
-const io=require('socket.io')(8000);
+const users = {};
 
-const users={};
-
-io.on('connection',socket=>{
- 
-    socket.on('new-user-joined',name=>{
-        console.log("New User",name);
-        users[socket.id]=name;
-        socket.broadcast.emit('user-joined',name);
+io.on('connection', socket =>{
+    // If any new user joins, let other users connected to the server know!
+    socket.on('new-user-joined', name =>{ 
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
     });
 
-    socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+    // If someone sends a message, broadcast it to other people
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     });
+
+    // If someone leaves the chat, let others know 
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+
+
 })
-
 // here io.on will set the event name as name whenever an event will be accepted
 
 //socket.boradcast.emit() will let everyone know is a new user jas joined
